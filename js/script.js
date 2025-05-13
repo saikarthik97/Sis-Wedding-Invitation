3e704ad63d7182359971d3e820d552d0c9cb27a3
sai karthik <sai150997Wgmail.com>
2025-05-14 00:30:52 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,18 @@
-/**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
+/**  CHARLES PLS MAKE THESE CHANGES
+ * 1. MANCHI AUDIO BGM PETTU LESS VOLUME LO
+ * 2. ui EMAINA MANCHIGAA MARCHITHE MARCHU red roses or aa white space ekkuvaga kanapadakunda fill ayyelaaga.
+ * 3. ADD COUNTER DATE TO JUN 4TH 7pm ki
+ * 4. inkemaina manchigaa cheyyagaligithe cheyyu
  */
+
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
 })(jQuery);
 
-/**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
- * Choose a heart-touching track! 🎵 ❤️
- *
- * Listen here: https://youtu.be/X0MDALpV29s
- *
- */
 $(document).on('click', function(){
-    document.getElementById("C:\Users\hp\OneDrive\Desktop\Wedding\Sis-Wedding-Invitation\assets\mp3\ - Srirasthu Subhamastu - SenSongsMp3.Co.mp3").play();
+    let audio = document.getElementById("emaina manchi Audio BGM add cheyyu ra").play();
+    audio.volume = 0.2;
 });
 
 var styles = [
